dbc570c38a68eab00b09ab8e165c9729daca9d1e
Jonathan Von Oppen <au630524@uni.au.dk>
2023-01-31 15:12:56 +0100

    join locations to address book

--- scripts/2023-01_contacts_map.R
+++ scripts/2023-01_contacts_map.R
@@ -27,7 +27,8 @@ pacman::p_load(sf,
 # define colours
 
 # File paths
-if(!dir.exists(file.path("data", "geonames"))) dir.create(file.path("data", "geonames"))
+geonames_path <- file.path("data", "geonames")
+if(!dir.exists(geonames_path)) dir.create(geonames_path)
 
 # country outlines from Eurostat
 europe_map_df <- read_sf("https://gisco-services.ec.europa.eu/distribution/v2/nuts/geojson/NUTS_RG_01M_2021_3035_LEVL_1.geojson") |> 
@@ -52,7 +53,7 @@ europe_elev <- elevatr::get_elev_raster(locations = europe_map_df,
   rename("elev" = 3)
 
 
-# Address book
+# Address book ----
 addresses <- rbind(
   c("Achim von Oppen & Christine Wedhorn", "Familie", "Berlin, DE"),
   c("Alex Dombrowski & Mela Padilla", "Schule, Stuttgart", "Stuttgart, DE"),
@@ -60,22 +61,23 @@ addresses <- rbind(
   c("Angela Prendin", "Phd, Aarhus", "Padova, IT"),
   c("Angelika & Eberhard von Oppen", "Familie", "Stuttgart, DE"),
   c("Anne Bjorkman", "PhD", "Göteborg, SE"),
-  c("Benjamin Richter", "Familie", "Marburg, DE"),
+  c("Benjamin Richter", "Familie", "Marburg an der Lahn, DE"),
   c("Caroline Saam", "Ultimate", "Köln, DE"),
+  c("Chiara Esposito", "WG, Aarhus", "Århus, DK"),
   c("Dagmar Egelkraut", "PhD", "Bergen, NO"),
   c("Dominik Jockers", "Ultimate", "Heidelberg, DE"),
-  c("Elena Pearce & Charlie Davison", "PhD, Aarhus", "Aarhus, DK"),
+  c("Elena Pearce & Charlie Davison", "PhD, Aarhus", "Århus, DK"),
   c("Elin Lindén", "Auslandssemester, Abisko", "Umeå, SE"),
   c("Elisabeth Seitz", "Familie", "Köln, DE"),
   c("Ellen & Chris Sommer", "Ultimate, Tübingen", "Bonn, DE"),
   c("Elli, Paul & Lukas Fehling", "Ultimate, Tübingen", "Tübingen, DE"),
   c("Emily Pickering Pedersen", "Auslandssemester, Abisko", "Abisko, SE"),
-  c("Flo Krüger", "Ultimate", "Marburg, DE"),
+  c("Flo Krüger", "Ultimate", "Marburg an der Lahn, DE"),
   c("Giulia Mazzotti", "Master, Davos", "Grenoble, FR"),
   c("Hanna Frick", "Studium, Tübingen", "Zürich, CH"),
   c("Heidi & Hartmut Richter", "Familie", "Neu-Anspach, DE"),
-  c("Ida Dumon", "Ultimate", "Brandenburg, DE"),
-  c("Jakob Assmann & Nina Moffat", "PhD", "Zürich, DE"),
+  c("Ida Dumon", "Ultimate", "Brandenburg an der Havel, DE"),
+  c("Jakob Assmann & Nina Moffat", "PhD", "Zürich, CH"),
   c("Jakob de Maeyer", "Ultimate", "Münster, DE"),
   c("Jakob Dürrwächter", "Schule, Stuttgart", "Berlin, DE"),
   c("Johannes Domeier", "Auslandssemester, Umeå", "Malmö, SE"),
@@ -84,23 +86,24 @@ addresses <- rbind(
   c("Katja Weigl", "Ultimate", "Freiburg, DE"),
   c("Keno Franke & Theresa Purschke", "Ultimate", "Berlin, DE"),
   c("Linnéa Weitkamp", "WG, Tübingen", "Leipzig, DE"),
-  c("Lisa Andresen", "Auslandssemester, Abisko", "Greifswald"),
-  c("Mariana García Criado", "PhD", "Edinburgh, UK"),
+  c("Lisa Andresen", "Auslandssemester, Abisko", "Greifswald, DE"),
+  c("Mariana García Criado", "PhD", "Edinburgh, GB"),
   c("Max Schön & Lisa Emerich", "WG, Tübingen", "Heidelberg, DE"),
   c("Muriel Ehrbar & Nick Solenthaler", "Master, Riederfurka", "Winterthur, CH"),
-  c("Ollie Baines", "PhD, Aarhus", "Aarhus, DK"),
+  c("Nacho Montero Requena", "WG, Aarhus", "Århus, DK"),
+  c("Ollie Baines", "PhD, Aarhus", "Århus, DK"),
   c("Patrick Möhl", "Master, Davos", "Basel, CH"),
   c("Ragnhild Gya & Lars-Olav Hammer", "PhD", "Bergen, NO"),
   c("Ronja Wedegärtner", "Studium, Tübingen", "Oslo, NO"),
   c("Rosa Witty", "Studium, Tübingen", "Tübingen, DE"),
-  c("Rosel von Oppen", "Familie", "Marburg, DE"),
+  c("Rosel von Oppen", "Familie", "Marburg an der Lahn, DE"),
   c("Sara ten Brinke & Jonas Bothe", "Wohnprojekt", "Alfter, DE"),
-  c("Signe Lemcke", "WG, Aarhus", "Aarhus, DK"),
-  c("Sonja Lorenz", "Musik, Tübingen", "München, DE"),
+  c("Signe Lemcke", "WG, Aarhus", "Århus, DK"),
+  c("Sonja Lorenz", "Musik, Tübingen", "Munich, DE"),
   c("Sonja Wipf & Christian Rixen", "Master, Davos", "Davos, CH"),
   c("Sophie Monsarrat & Adam Custock", "PhD", "Nijmegen, NL"),
   c("Tofu (Markus) Starr", "Ultimate", "Hamburg, DE"),
-  c("Ulrich Schaffert", "Familie", "Frankfurt, DE"),
+  c("Ulrich Schaffert", "Familie", "Frankfurt am Main, DE"),
   c("Vera Blaschke & Flo Schunck", "Ultimate", "Leipzig, DE"),
   c("Vera Middendorf & Behrend Dellwisch", "WG, Tübingen", "Münster, DE")
 ) |> 
@@ -111,18 +114,61 @@ addresses <- rbind(
   # name columns
   rename(name = 1,
          connection = 2,
-         town = 3)
+         place = 3) |> 
+  
+  # separate place and country
+  separate(place,
+           sep = ", ",
+           into = c("place", "country"))
 
-addresses_locations <- download.file("http://download.geonames.org/export/dump/cities1000.zip") |> 
-  # unzip into data
-  zip::unzip(exdir = "data/geonames") #|> 
+
+# load city location data
+if(!file.exists(file.path(geonames_path, "cities1000.txt"))) {
+  download.file("http://download.geonames.org/export/dump/cities1000.zip", 
+                destfile = geonames_path)
+  zip::unzip(exdir = "data/geonames")
+  }
+
+# join locations to places
+addresses_locations <- read_delim(file = file.path("data", "geonames", "cities1000.txt"), 
+                                  col_names = c("geonameid", "name", "asciiname", "alternatenames", 
+                                                "latitude", "longitude",  # WGS84
+                                                "feature class", "feature code", 
+                                                "country code", "cc2", 
+                                                "admin1 code", "admin2 code", "admin3 code", "admin4 code",      # codes for 1st/2nd/3rd/4th administrative division
+                                                "population", 
+                                                "elevation", "dem",  # srtm3 or gtopo30, average elevation of 3''x3'' (ca 90mx90m) or 30''x30'' (ca 900mx900m) area in meters, integer.
+                                                "timezone",        # iana timezone id
+                                                "modification date")) %>% 
   
   # get coordinates through Geonames
-  right_join(addresses |> 
-              pull(town) |> 
-              GNsearch() |> 
-              bind_rows(),
-            by = c("town" = "city"))
+  left_join(x = addresses,
+            y = .,
+            by = c("place" = "name", "country" = "country code")) |> 
+  
+  # fix multiple matches for "Münster"
+  filter(!(place == "Münster" & population < 1e5)) |> 
+  
+  # fill in lat and lon manually for Abisko 
+  mutate(latitude = case_when(place == "Abisko" ~ 68.349347,
+                              TRUE ~ latitude),
+         longitude = case_when(place == "Abisko" ~ 18.831289,
+                               TRUE ~ longitude)) |> 
+  
+  # select relevant columns
+  select(name, connection, 
+         place, country, 
+         latitude, longitude) |> 
+  
+  # split connection column
+  separate(connection,
+           sep = ", ",
+           into = c("connection_background", "connection_place"),
+           extra = "merge", 
+           fill = "right") |> 
+  
+  # transform to sf object
+  st_as_sf(coords = c("longitude", "latitude"))
 
 
 # Plot map ----
